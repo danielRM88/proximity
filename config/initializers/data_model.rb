e64f331abd08a6c090fe364b70a2e8669424c0a8
Daniel Rosato <rosato.daniel@gmail.com>
2018-03-13 12:47:05 +0100

    added initializer

--- config/initializers/data_model.rb
+++ config/initializers/data_model.rb
@@ -1,27 +1,27 @@
 require 'descriptive_statistics'
 # require 'rootapp-rinruby'
-require 'rinruby'
-# if defined?(Rails::Server)
-#   if ENV['DATAMODEL'] == 'lg'
-#     # puts "Initialising and Training Logistic Regression Model"
-#     # LogisticRegression.init
-#     algorithm = LogisticRegression
-#     iterations = 2
-#   elsif ENV['DATAMODEL'] == 'as'
-#     algorithm = AverageSquare
-#     iterations = 1
-#   else
-#     algorithm = LogisticRegression
-#   end
+if defined?(Rails::Server)
+  require 'rinruby'
+  if ENV['DATAMODEL'] == 'lg'
+    # puts "Initialising and Training Logistic Regression Model"
+    # LogisticRegression.init
+    algorithm = LogisticRegression
+    iterations = 2
+  elsif ENV['DATAMODEL'] == 'as'
+    algorithm = AverageSquare
+    iterations = 1
+  else
+    algorithm = LogisticRegression
+  end
 
-#   calibration = Calibration.last
-#   if calibration.blank?
-#     Calibration.create(records: algorithm.records_needed_for_calibration, algorithm_class: algorithm.to_s, iterations: iterations)
-#   elsif calibration.algorithm_class != algorithm.to_s
-#     Calibration.destroy_all
-#     Calibration.create(records: algorithm.records_needed_for_calibration, algorithm_class: algorithm.to_s, iterations: iterations)
-#   else
-#     calibration.set_algorithm algorithm
-#     calibration.save
-#   end
-# end
+  calibration = Calibration.last
+  if calibration.blank?
+    Calibration.create(records: algorithm.records_needed_for_calibration, algorithm_class: algorithm.to_s, iterations: iterations)
+  elsif calibration.algorithm_class != algorithm.to_s
+    Calibration.destroy_all
+    Calibration.create(records: algorithm.records_needed_for_calibration, algorithm_class: algorithm.to_s, iterations: iterations)
+  else
+    calibration.set_algorithm algorithm
+    calibration.save
+  end
+end
